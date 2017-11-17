7aae45d6050e97f9220c3a973249a682887c869e
Katalin Lovagné Szűcs <kszucs@monguz.hu>
2017-11-17 13:31:11 +0100

    UICAL-1 Make events visible

--- Calendar.js
+++ Calendar.js
@@ -8,38 +8,23 @@ import BigCalendar from '@folio/react-big-calendar';
 import moment from 'moment';
 import '!style-loader!css-loader!./css/react-big-calendar.css';
 import '!style-loader!css-loader!./css/folio-calendar.css';
-import events from './events';
+import calendarEvents from './events';
 
 class Calendar extends React.Component {
 
-  static contextTypes = {
-    stripes: PropTypes.object,
-  }
-
-  constructor(props, context) {
-    super(props);
-    this.okapiUrl = context.stripes.okapi.url;
-    this.httpHeaders = Object.assign({}, {
-      'X-Okapi-Tenant': context.stripes.okapi.tenant,
-      'X-Okapi-Token': context.stripes.store.getState().okapi.token,
-      'Content-Type': 'application/json',
-    });
-  }
-
   render() {
      let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
-     BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment));
      BigCalendar.momentLocalizer(moment);
 
      return (
       <Paneset>
-        <Pane id="pane-calendar" defaultWidth="fill" fluidContentWidth paneTitle='Institutional calendar'>
+        <Pane id="pane-calendar" defaultWidth="fill" height="100%" fluidContentWidth paneTitle='Institutional calendar'>
           <BigCalendar
-            {...this.props.stripes}
-            events={events}
-            startAccessor="startDate"
-            endAccessor="endDate"
-            defaultDate={new Date(2015, 3, 1)}
+            {...this.props}
+            events={calendarEvents}
+            startAccessor="start"
+            endAccessor="end"
+            defaultDate={new Date(2017, 3, 1)}
             views={allViews}
           />
         </Pane>
--- events.js
+++ events.js
@@ -1,80 +1,60 @@
 export default [
-    {
-        'title': 'All Day Event',
-        'allDay': true,
-        'start': new Date(2015, 3, 0),
-        'end': new Date(2015, 3, 1)
-    },
-    {
-        'title': 'Long Event',
-        'start': new Date(2015, 3, 7),
-        'end': new Date(2015, 3, 10)
-    },
+  {
+    title: 'Opening day',
+    start: new Date(2017, 3, 2, 8, 30, 0, 0),
+    end: new Date(2017, 3, 2, 17, 30, 0, 0),
+  },
+  {
+    title: 'Opening day',
+    start: new Date(2017, 3, 3, 8, 30, 0, 0),
+    end: new Date(2017, 3, 3, 17, 30, 0, 0),
+  },
+  {
+    title: 'Opening day',
+    start: new Date(2017, 3, 4, 8, 30, 0, 0),
+    end: new Date(2017, 3, 4, 17, 30, 0, 0),
+  },
+  {
+    title: 'Opening day',
+    start: new Date(2017, 3, 5, 8, 30, 0, 0),
+    end: new Date(2017, 3, 5, 17, 30, 0, 0),
+  },
+  {
+    title: 'Opening day',
+    start: new Date(2017, 3, 6, 8, 30, 0, 0),
+    end: new Date(2017, 3, 6, 17, 30, 0, 0),
+  },
 
-    {
-        'title': 'DTS STARTS',
-        'start': new Date(2016, 2, 13, 0, 0, 0),
-        'end': new Date(2016, 2, 20, 0, 0, 0)
-    },
+  {
+    title: 'Opening day',
+    start: new Date(2017, 3, 9, 8, 30, 0, 0),
+    end: new Date(2017, 3, 9, 17, 30, 0, 0),
+  },
+  {
+    title: 'Opening day',
+    start: new Date(2017, 3, 10, 8, 30, 0, 0),
+    end: new Date(2017, 3, 10, 17, 30, 0, 0),
+  },
+  {
+    title: 'Opening day',
+    start: new Date(2017, 3, 11, 8, 30, 0, 0),
+    end: new Date(2017, 3, 11, 17, 30, 0, 0),
+  },
+  {
+    title: 'Opening day',
+    start: new Date(2017, 3, 12, 8, 30, 0, 0),
+    end: new Date(2017, 3, 12, 17, 30, 0, 0),
+  },
+  {
+    title: 'Opening day',
+    start: new Date(2017, 3, 13, 8, 30, 0, 0),
+    end: new Date(2017, 3, 13, 17, 30, 0, 0),
+  },
+  {
+    title: 'Closing days',
+    allDay: true,
+    start: new Date(2017, 3, 16),
+    end: new Date(2017, 3, 18, 23, 59, 59),
+  },
 
-    {
-        'title': 'DTS ENDS',
-        'start': new Date(2016, 10, 6, 0, 0, 0),
-        'end': new Date(2016, 10, 13, 0, 0, 0)
-    },
-
-    {
-        'title': 'Some Event',
-        'start': new Date(2015, 3, 9, 0, 0, 0),
-        'end': new Date(2015, 3, 9, 0, 0, 0)
-    },
-    {
-        'title': 'Conference',
-        'start': new Date(2015, 3, 11),
-        'end': new Date(2015, 3, 13),
-        desc: 'Big conference for important people'
-    },
-    {
-        'title': 'Meeting',
-        'start': new Date(2015, 3, 12, 10, 30, 0, 0),
-        'end': new Date(2015, 3, 12, 12, 30, 0, 0),
-        desc: 'Pre-meeting meeting, to prepare for the meeting'
-    },
-    {
-        'title': 'Lunch',
-        'start':new Date(2015, 3, 12, 12, 0, 0, 0),
-        'end': new Date(2015, 3, 12, 13, 0, 0, 0),
-        desc: 'Power lunch'
-    },
-    {
-        'title': 'Meeting',
-        'start':new Date(2015, 3, 12,14, 0, 0, 0),
-        'end': new Date(2015, 3, 12,15, 0, 0, 0)
-    },
-    {
-        'title': 'Happy Hour',
-        'start':new Date(2015, 3, 12, 17, 0, 0, 0),
-        'end': new Date(2015, 3, 12, 17, 30, 0, 0),
-        desc: 'Most important meal of the day'
-    },
-    {
-        'title': 'Dinner',
-        'start':new Date(2015, 3, 12, 20, 0, 0, 0),
-        'end': new Date(2015, 3, 12, 21, 0, 0, 0)
-    },
-    {
-        'title': 'Birthday Party',
-        'start':new Date(2015, 3, 13, 7, 0, 0),
-        'end': new Date(2015, 3, 13, 10, 30, 0)
-    },
-    {
-        'title': 'Late Night Event',
-        'start':new Date(2015, 3, 17, 19, 30, 0),
-        'end': new Date(2015, 3, 18, 2, 0, 0)
-    },
-    {
-        'title': 'Multi-day Event',
-        'start':new Date(2015, 3, 20, 19, 30, 0), 
-        'end': new Date(2015, 3, 22, 2, 0, 0)
-    }
-]
+];
