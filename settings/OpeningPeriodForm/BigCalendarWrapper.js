f8fb55a19e5e1bb4422859884a8f22ae3e743658
Anna_Melnyk <Anna_Melnyk@epam.com>
2019-12-05 12:27:44 +0200

    UICAL-82: Prevent event's dublicate creation

--- settings/OpeningPeriodForm/BigCalendarWrapper.js
+++ settings/OpeningPeriodForm/BigCalendarWrapper.js
@@ -10,6 +10,8 @@ import {
   momentLocalizer,
 } from 'react-big-calendar';
 
+import { Callout } from '@folio/stripes/components';
+
 import CalendarUtils from '../../CalendarUtils';
 import EventComponent from '../../components/EventComponent';
 
@@ -85,6 +87,10 @@ class BigCalendarWrapper extends React.PureComponent {
     }
 
     onEventDnD = ({ event, start, end, isAllDay: droppedOnAllDaySlot }) => {
+      if (!this.preventEventDublication(start, end)) {
+        return;
+      }
+
       const { events } = this.state;
       const isSameDay = start.getDay() === end.getDay();
       const idx = events.indexOf(event);
@@ -122,11 +128,34 @@ class BigCalendarWrapper extends React.PureComponent {
       this.onCalendarChange(nextEvents);
     };
 
+    preventEventDublication = (startTime, endTime) => {
+      const { events } = this.state;
+      const existingEventsArr = events.map(event => {
+        const { start, end } = event;
+        const startOfExistedEvent = moment(start);
+        const endOfExistedEvent = moment(end);
+        const startTimeMoment = moment(startTime);
+        const endTimeMoment = moment(endTime);
+
+        if (startTimeMoment.isSame(startOfExistedEvent) && endTimeMoment.isSame(endOfExistedEvent)) {
+          return false;
+        }
+        return true;
+      });
+
+      return existingEventsArr.every(event => event);
+    }
+
     onSlotSelect = ({ start, end }) => {
+      if (!this.preventEventDublication(start, end)) {
+        return;
+      }
+
       const { eventIdCounter } = this.state;
       const isAllDay = start === end;
       const id = eventIdCounter + 1;
       const events = [...this.state.events];
+
 
       events.push({
         id,
