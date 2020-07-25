759b874bb8c417e1a42743aa8553e08143bc85a5
Min Kim <minkimcello@gmail.com>
2020-07-25 10:22:32 -0400

    Refactor to MirageJS (#280)
    * Remove bigtest/mirage and add inflected, faker, miragejs
    
    * Configure server type as miragejs for stripes
    
    * Refactor factories
    
    * Refactor models
    
    * Refactor serializers
    
    * Refactor the remaining inflected and faker functions
    
    * Update CHANGELOG

--- test/bigtest/network/serializers/okapi.js
+++ test/bigtest/network/serializers/okapi.js
@@ -1,14 +1,15 @@
 import {
   camelize,
   pluralize,
-} from '@bigtest/mirage';
+  underscore
+} from 'inflected';
 
 import ApplicationSerializer from './application';
 
 export default ApplicationSerializer.extend({
   serialize(...args) {
     const { modelName } = args[0];
-    const collectionName = pluralize(camelize(modelName));
+    const collectionName = pluralize(camelize(underscore(modelName), false));
     const name = this.name || collectionName;
     const json = ApplicationSerializer.prototype.serialize.apply(this, args);
     const data = json[collectionName];
