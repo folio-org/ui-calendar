f631b701de1faa7ee5d372f897159aca74a1fe02
Noah Overcash <ncovercash@ua.edu>
2022-06-16 21:21:44 -0500

    Delete tests folder

--- test/bigtest/interactors/KeyValue.js
+++
@@ -1,9 +0,0 @@
-import {
-  interactor,
-  scoped,
-} from '@bigtest/interactor';
-
-export default interactor(class KeyValue {
-  label = scoped('[class^="kvRoot-"] div');
-  value = scoped('[class^="kvRoot-"] div:nth-child(2)');
-});
