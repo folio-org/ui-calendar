003e22ccf8290e6f1d0b94fd79c5d40c43060e15
Noah Overcash <ncovercash@ua.edu>
2022-09-18 15:54:58 -0500

    Fix quotes

--- src/utils/DateUtils.monthComparison.test.ts
+++ src/utils/DateUtils.monthComparison.test.ts
@@ -1,7 +1,7 @@
-import { isSameMonth, isSameMonthOrBefore } from "./DateUtils"
-import * as Dates from "../test/data/Dates";
+import { isSameMonth, isSameMonthOrBefore } from './DateUtils';
+import * as Dates from '../test/data/Dates';
 
-test("isSameMonth works as expected", () => {
+test('isSameMonth works as expected', () => {
   expect(isSameMonth(Dates.MAY_14_DATE, Dates.MAY_2_DATE)).toBe(true);
   expect(isSameMonth(Dates.MAY_14_DATE, Dates.MAY_14_2001_DATE)).toBe(false);
   expect(isSameMonth(Dates.JUL_1_DATE, Dates.MAY_14_2001_DATE)).toBe(false);
@@ -11,16 +11,24 @@ test("isSameMonth works as expected", () => {
   expect(isSameMonth(Dates.MAY_14_2001_DATE, Dates.MAY_14_DATE)).toBe(false);
   expect(isSameMonth(Dates.MAY_14_2001_DATE, Dates.JUL_1_DATE)).toBe(false);
   expect(isSameMonth(Dates.MAY_14_DATE, Dates.JUL_1_DATE)).toBe(false);
-})
+});
 
-test("isSameMonthOrBefore works as expected", () => {
+test('isSameMonthOrBefore works as expected', () => {
   expect(isSameMonthOrBefore(Dates.MAY_14_DATE, Dates.MAY_2_DATE)).toBe(true);
-  expect(isSameMonthOrBefore(Dates.MAY_14_DATE, Dates.MAY_14_2001_DATE)).toBe(true);
-  expect(isSameMonthOrBefore(Dates.JUL_1_DATE, Dates.MAY_14_2001_DATE)).toBe(true);
+  expect(isSameMonthOrBefore(Dates.MAY_14_DATE, Dates.MAY_14_2001_DATE)).toBe(
+    true
+  );
+  expect(isSameMonthOrBefore(Dates.JUL_1_DATE, Dates.MAY_14_2001_DATE)).toBe(
+    true
+  );
   expect(isSameMonthOrBefore(Dates.JUL_1_DATE, Dates.MAY_14_DATE)).toBe(false);
 
   expect(isSameMonthOrBefore(Dates.MAY_2_DATE, Dates.MAY_14_DATE)).toBe(true);
-  expect(isSameMonthOrBefore(Dates.MAY_14_2001_DATE, Dates.MAY_14_DATE)).toBe(false);
-  expect(isSameMonthOrBefore(Dates.MAY_14_2001_DATE, Dates.JUL_1_DATE)).toBe(false);
+  expect(isSameMonthOrBefore(Dates.MAY_14_2001_DATE, Dates.MAY_14_DATE)).toBe(
+    false
+  );
+  expect(isSameMonthOrBefore(Dates.MAY_14_2001_DATE, Dates.JUL_1_DATE)).toBe(
+    false
+  );
   expect(isSameMonthOrBefore(Dates.MAY_14_DATE, Dates.JUL_1_DATE)).toBe(true);
-})
+});
