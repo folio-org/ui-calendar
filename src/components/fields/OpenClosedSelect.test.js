3c1d4871d0e37b97cb5e9015faa86fc5330bf222
Zak Burke <zburke@ebsco.com>
2022-09-26 16:04:43 -0400

    use withIntlConfiguration instead of react-intl mock

--- src/components/fields/OpenClosedSelect.test.js
+++ src/components/fields/OpenClosedSelect.test.js
@@ -3,59 +3,33 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import RowType from './RowType';
-
 import OpenClosedSelect from './OpenClosedSelect';
-
-jest.mock('react-intl', () => {
-  const intl = {
-    formatMessage: ({ id }) => id,
-  };
-
-  return {
-    ...jest.requireActual('react-intl'),
-    FormattedMessage: jest.fn(({ id, children }) => {
-      if (children) {
-        return children([id]);
-      }
-
-      return id;
-    }),
-    FormattedTime: jest.fn(({ value, children }) => {
-      if (children) {
-        return children([value]);
-      }
-
-      return value;
-    }),
-    useIntl: () => intl,
-    injectIntl: (Component) => (props) => <Component {...props} intl={intl} />,
-  };
-});
+import withIntlConfiguration from '../../test/util/withIntlConfiguration';
 
 describe('OpenClosedSelect', () => {
   it('includes open', () => {
     const onBlur = jest.fn();
     const onChange = jest.fn();
 
-    render(<OpenClosedSelect onBlur={onBlur} onChange={onChange} />);
+    render(withIntlConfiguration(<OpenClosedSelect onBlur={onBlur} onChange={onChange} />));
 
-    expect(screen.getByText('ui-calendar.calendarForm.openClosedSelect.open')).toBeInTheDocument();
+    expect(screen.getByText('Open')).toBeInTheDocument();
   });
 
   it('includes close', () => {
     const onBlur = jest.fn();
     const onChange = jest.fn();
 
-    render(<OpenClosedSelect onBlur={onBlur} onChange={onChange} />);
+    render(withIntlConfiguration(<OpenClosedSelect onBlur={onBlur} onChange={onChange} />));
 
-    expect(screen.getByText('ui-calendar.calendarForm.openClosedSelect.closed')).toBeInTheDocument();
+    expect(screen.getByText('Closed')).toBeInTheDocument();
   });
 
   it('calls onBlur and onChange callbacks', () => {
     const onBlur = jest.fn();
     const onChange = jest.fn();
 
-    render(<OpenClosedSelect onBlur={onBlur} onChange={onChange} />);
+    render(withIntlConfiguration(<OpenClosedSelect onBlur={onBlur} onChange={onChange} />));
     userEvent.selectOptions(screen.getByRole('combobox'), [RowType.Open]);
 
     expect(onBlur).toHaveBeenCalled();
