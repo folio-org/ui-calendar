e148673fb23c04833bcbf25e847f9b5c045a59cc
Zak Burke <zburke@ebsco.com>
2022-09-26 19:56:23 -0400

    constants FTW

--- src/components/fields/WeekdayPicker.test.js
+++ src/components/fields/WeekdayPicker.test.js
@@ -3,11 +3,12 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import withIntlConfiguration from '../../test/util/withIntlConfiguration';
+import * as Weekdays from '../../test/data/Weekdays';
 import WeekdayPicker from './WeekdayPicker';
 
 describe('WeekdayPicker', () => {
   it('correctly calls props.onChange', () => {
-    const day = 'SUNDAY';
+    const day = Weekdays.Sunday;
     const onChange = jest.fn();
     render(withIntlConfiguration(<WeekdayPicker onChange={onChange} />));
 
