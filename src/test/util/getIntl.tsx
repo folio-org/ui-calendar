10cc6fdbbdfa4c784440c97f069042195bce64bc
Noah Overcash <ncovercash@ua.edu>
2022-09-26 15:05:37 -0500

    Memoize getIntl helper

--- src/test/util/getIntl.tsx
+++ src/test/util/getIntl.tsx
@@ -1,20 +1,24 @@
 import { cleanup, render } from '@testing-library/react';
+import memoizee from 'memoizee';
 import React, { FunctionComponent } from 'react';
 import { IntlContext, IntlShape } from 'react-intl';
 import withIntlConfiguration from './withIntlConfiguration';
 
-export default function getIntl(locale = 'en-US', timeZone = 'UTC'): IntlShape {
-  const intlCapturer = jest.fn();
+export default memoizee(
+  (locale = 'en-US', timeZone = 'UTC'): IntlShape => {
+    const intlCapturer = jest.fn();
 
-  const TestComponent: FunctionComponent<Record<string, never>> = () => (
-    <IntlContext.Consumer>{intlCapturer}</IntlContext.Consumer>
-  );
-  render(withIntlConfiguration(<TestComponent />, locale, timeZone));
+    const TestComponent: FunctionComponent<Record<string, never>> = () => (
+      <IntlContext.Consumer>{intlCapturer}</IntlContext.Consumer>
+    );
+    render(withIntlConfiguration(<TestComponent />, locale, timeZone));
 
-  expect(intlCapturer).toHaveBeenCalled();
-  const intl = intlCapturer.mock.calls[0][0] as IntlShape;
+    expect(intlCapturer).toHaveBeenCalled();
+    const intl = intlCapturer.mock.calls[0][0] as IntlShape;
 
-  cleanup();
+    cleanup();
 
-  return intl;
-}
+    return intl;
+  },
+  { length: 2 }
+);
