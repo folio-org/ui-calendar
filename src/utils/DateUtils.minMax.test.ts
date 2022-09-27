88cb754e310776062ce3d8b7d8af543221cfa788
William <wmholland@crimson.ua.edu>
2022-09-27 12:57:42 -0500

    DateUtils to 100% coverage

---
+++ src/utils/DateUtils.minMax.test.ts
@@ -0,0 +1,24 @@
+import * as Dates from '../test/data/Dates';
+import { minDate, maxDate, isBetweenDatesByDay } from './DateUtils';
+
+test('min and max date work for null', () => {
+  expect(minDate([])).toBe(null);
+  expect(maxDate([])).toBe(null);
+});
+
+test('min and max date work for normal input', () => {
+  expect(minDate([Dates.JAN_1_DATE])).toStrictEqual(Dates.JAN_1_DATE);
+  expect(maxDate([Dates.JAN_1_DATE])).toStrictEqual(Dates.JAN_1_DATE);
+});
+
+test('min and max date work for normal input', () => {
+  expect(minDate([Dates.JAN_1_DATE, Dates.DEC_1_2001_DATE, Dates.DEC_1_DATE, Dates.MAY_14_2001_DATE])).toStrictEqual(Dates.JAN_1_DATE);
+  expect(minDate([Dates.DEC_1_2001_DATE, Dates.DEC_1_DATE, Dates.MAY_14_2001_DATE])).toStrictEqual(Dates.DEC_1_DATE);
+  expect(maxDate([Dates.JAN_1_DATE, Dates.DEC_1_2001_DATE, Dates.DEC_1_DATE, Dates.MAY_14_2001_DATE])).toStrictEqual(Dates.DEC_1_2001_DATE);
+});
+
+test('isBetweenDatesByDay', () => {
+  expect(isBetweenDatesByDay(Dates.JAN_1_DATE, Dates.MAR_1_DATE, Dates.DEC_17_DATE)).toBe(false);
+  expect(isBetweenDatesByDay(Dates.MAR_1_DATE, Dates.JAN_1_DATE, Dates.DEC_17_DATE)).toBe(true);
+  expect(isBetweenDatesByDay(Dates.DEC_17_DATE, Dates.MAY_14_DATE, Dates.MAY_14_2001_DATE)).toBe(true);
+});
