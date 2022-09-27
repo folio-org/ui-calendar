3288caf8e851fcfa644a885316958ed62ee028ad
Noah Overcash <ncovercash@ua.edu>
2022-09-27 18:33:51 -0500

    Add index tests

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,15 @@
-import type { SettingsProps } from '@folio/stripes/smart-components';
 import type { FunctionComponent } from 'react';
 import React from 'react';
-import Settings from './views';
+import Settings, { CalendarSettingsProps } from './views/CalendarSettings';
 
-const CalendarRouting: FunctionComponent<SettingsProps> = (props) => {
+const CalendarRouting: FunctionComponent<CalendarSettingsProps> = (props) => {
   if (props.showSettings) {
     return <Settings {...props} />;
   } else {
     return (
       <div>
         <h2>Uh-oh!</h2>
-        <p>
-          How did you get to
-          {props.location.pathname}?
-        </p>
+        <p>How did you get to {props.location.pathname}?</p>
       </div>
     );
   }
