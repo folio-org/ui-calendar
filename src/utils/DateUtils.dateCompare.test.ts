cab0d974706578255020e66d07cfcf4434beee7b
William <wmholland@crimson.ua.edu>
2022-09-27 13:02:19 -0500

    Moved isBetween to dateCompare file

--- src/utils/DateUtils.dateCompare.test.ts
+++ src/utils/DateUtils.dateCompare.test.ts
@@ -1,5 +1,5 @@
 import * as Dates from '../test/data/Dates';
-import { dateCompare } from './DateUtils';
+import { dateCompare, isBetweenDatesByDay} from './DateUtils';
 
 describe('dateCompare utility function', () => {
   it('Date comparison works as expected', () => {
@@ -13,4 +13,10 @@ describe('dateCompare utility function', () => {
     expect(dateCompare(undefined, Dates.DEC_1_DATE)).toBe(-1);
     expect(dateCompare(Dates.DEC_1_DATE, undefined)).toBe(1);
   });
+});
+
+test('isBetweenDatesByDay', () => {
+  expect(isBetweenDatesByDay(Dates.JAN_1_DATE, Dates.MAR_1_DATE, Dates.DEC_17_DATE)).toBe(false);
+  expect(isBetweenDatesByDay(Dates.MAR_1_DATE, Dates.JAN_1_DATE, Dates.DEC_17_DATE)).toBe(true);
+  expect(isBetweenDatesByDay(Dates.DEC_17_DATE, Dates.MAY_14_DATE, Dates.MAY_14_2001_DATE)).toBe(true);
 });
