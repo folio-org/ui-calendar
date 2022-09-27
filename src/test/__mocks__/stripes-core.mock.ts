a694985b08a0cf98af2723d0db02e95925795bf1
Zak Burke <zburke@ebsco.com>
2022-09-27 17:32:40 -0400

    UICAL-206 test AllCalendarView
    Test `./src/views/AllCalendarView`.
    
    * Elaborate stripes-core mock copied mostly verbatim from ui-users,
      minus a few things TS barfed on.
    * `withHistoryConfiguration` mirrors `withIntlConfiguration` but
      provides a react-router provider.
    * Minor bug fix: localize the "All calendars" pane title
    
    Refs UICAL-206

--- src/test/__mocks__/stripes-core.mock.ts
+++ src/test/__mocks__/stripes-core.mock.ts
@@ -1,4 +1,110 @@
-jest.mock('@folio/stripes/core', () => ({
-  ...jest.requireActual('@folio/stripes-core'),
-  useOkapiKy: jest.fn()
-}));
+// jest.mock('@folio/stripes/core', () => ({
+//   ...jest.requireActual('@folio/stripes-core'),
+//   useOkapiKy: jest.fn(),
+//   useStripes: () => ({
+//     hasPerm: jest.fn().mockReturnValue(true),
+//   })
+// }));
+
+jest.mock('@folio/stripes/core', () => {
+  const STRIPES = {
+    actionNames: [],
+    bindings: {},
+    clone: jest.fn(),
+    config: {
+      logCategories: '',
+      logPrefix: '',
+      logTimestamp: true,
+      showPerms: true,
+      showHomeLink: true,
+      listInvisiblePerms: true,
+      disableAuth: false,
+      hasAllPerms: false,
+    },
+    currency: 'USD',
+    discovery: {
+      interfaces: {},
+      isFinished: true,
+      modules: {},
+      okapi: {},
+    },
+    epics: {
+      add: jest.fn(),
+      middleware: jest.fn(),
+    },
+    hasInterface: () => true,
+    hasPerm: () => true,
+    icons: {},
+    locale: 'en-US',
+    logger: {
+      log: () => {},
+    },
+    metadata: {},
+    okapi: {
+      authFailure: false,
+      okapiReady: true,
+      tenant: 'diku',
+      token: 'c0ffee',
+      translations: {
+        'stripes-components.Datepicker.calendar': 'calendar',
+        'stripes-components.Datepicker.calendarDaysList': 'calendar days list.',
+        'stripes-core.button.cancel': [{ type: 0, value: 'Cancel' }],
+        'ui-users.permission.modal.list.pane.header': 'Permissions',
+        'ui-users.permission.modal.list.pane.header.array': [{ type: 0, value: 'Permissions' }],
+        default: false,
+      },
+      url: 'https://folio-testing-okapi.dev.folio.org',
+      withoutOkapi: false,
+    },
+    plugins: {},
+    setBindings: jest.fn(),
+    setCurrency: jest.fn(),
+    setLocale: jest.fn(),
+    setSinglePlugin: jest.fn(),
+    setTimezone: jest.fn(),
+    setToken: jest.fn(),
+    store: {
+      getState: () => ({
+        okapi: {
+          token: 'abc',
+        },
+      }),
+      dispatch: () => {},
+      subscribe: () => {},
+      replaceReducer: () => {},
+    },
+    timezone: 'UTC',
+    user: {
+      perms: {},
+      user: {
+        addresses: [],
+        firstName: 'Testy',
+        lastName: 'McTesterson',
+        email: 'test@folio.org',
+        id: 'b1add99d-530b-5912-94f3-4091b4d87e2c',
+        username: 'diku_admin',
+      },
+    },
+    withOkapi: true,
+  };
+
+  return {
+    ...jest.requireActual('@folio/stripes/core'),
+    IfInterface: jest.fn(({ name, children }) => {
+      return name === 'interface' || name === 'service-points-users' ? children : null;
+    }),
+    IfPermission: jest.fn(({ perm, children }) => {
+      if (perm === 'permission') {
+        return children;
+      } else if (perm.startsWith('ui-calendar')) {
+        return children;
+      } else if (perm.startsWith('perms')) {
+        return children;
+      } else {
+        return null;
+      }
+    }),
+    Pluggable: jest.fn(({ children }) => [children]),
+    useStripes: () => STRIPES,
+  };
+});
---
+++ src/test/util/withHistoryConfiguration.tsx
@@ -0,0 +1,56 @@
+import stripesComponentsTranslations from '@folio/stripes-components/translations/stripes-components/en';
+import stripesCoreTranslations from '@folio/stripes-core/translations/stripes-core/en';
+import React, { ReactNode } from 'react';
+import { IntlProvider } from 'react-intl';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+// cannot make TS happy without the .json
+// eslint-disable-next-line import/extensions
+import localTranslations from '../../../translations/ui-calendar/en.json';
+
+const translationSets = [
+  {
+    prefix: 'ui-calendar',
+    translations: localTranslations,
+  },
+  {
+    prefix: 'stripes-components',
+    translations: stripesComponentsTranslations,
+  },
+  {
+    prefix: 'stripes-core',
+    translations: stripesCoreTranslations,
+  },
+];
+
+function withHistoryConfiguration(
+  children: ReactNode,
+  locale = 'en-US',
+  timeZone = 'UTC'
+): JSX.Element {
+  const allTranslations: Record<string, string> = {};
+
+  translationSets.forEach((set) => {
+    const { prefix, translations } = set;
+    Object.keys(translations).forEach((key) => {
+      allTranslations[`${prefix}.${key}`] = translations[key];
+    });
+  });
+
+  const history = createMemoryHistory();
+
+  return (
+    <Router history={history}>
+      <IntlProvider
+        locale={locale}
+        timeZone={timeZone}
+        messages={allTranslations}
+      >
+        {children}
+      </IntlProvider>
+    </Router>
+  );
+}
+
+export default withHistoryConfiguration;
