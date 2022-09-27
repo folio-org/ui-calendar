8f10b23d029696f393891422d53e5baea9cca3f2
Noah Overcash <ncovercash@ua.edu>
2022-09-27 11:37:24 -0500

    await user events

--- src/components/fields/WeekdayPicker.test.tsx
+++ src/components/fields/WeekdayPicker.test.tsx
@@ -7,13 +7,17 @@ import * as Weekdays from '../../test/data/Weekdays';
 import WeekdayPicker from './WeekdayPicker';
 
 describe('WeekdayPicker', () => {
-  it('correctly calls props.onChange', () => {
+  it('correctly calls props.onChange', async () => {
     const day = Weekdays.Sunday;
     const onChange = jest.fn();
-    render(withIntlConfiguration(<WeekdayPicker onChange={onChange} />));
+    render(
+      withIntlConfiguration(
+        <WeekdayPicker value={undefined} onChange={onChange} />
+      )
+    );
 
-    userEvent.selectOptions(screen.getByRole('combobox'), [day]);
-    userEvent.selectOptions(screen.getByRole('combobox'), ['']);
+    await userEvent.selectOptions(screen.getByRole('combobox'), [day]);
+    await userEvent.selectOptions(screen.getByRole('combobox'), ['']);
 
     expect(onChange).toHaveBeenNthCalledWith(1, day);
     expect(onChange).toHaveBeenNthCalledWith(2, undefined);
--- src/views/CalendarSettings.test.tsx
+++ src/views/CalendarSettings.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { SettingsProps } from '@folio/stripes/smart-components';
 import CalendarSettings from './CalendarSettings';
 
 jest.mock('@folio/stripes-smart-components/lib/Settings', () => jest.fn().mockReturnValue('Settings'));
 
-const renderCalendarSettings = () => render(<CalendarSettings />);
+const renderCalendarSettings = () => render(<CalendarSettings {...({} as unknown as SettingsProps)} />);
 
 describe('Calender Settings', () => {
   it('should render Calendar Settings', () => {
