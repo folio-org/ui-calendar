c1e75a1430dbc01d1d02a2b14917ee7751605628
Priyanka <priyanka_terala@epam.com>
2022-09-27 16:41:33 +0530

    UICAL-209 - Jest/RTL test views/CalendarSettings

--- src/test/__mocks__/stripes-core.mock.ts
+++ src/test/__mocks__/stripes-core.mock.ts
@@ -1,4 +1,4 @@
-jest.mock('@folio/stripes-core/src/components', () => ({}));
+// jest.mock('@folio/stripes-core/src/components', () => ({}));
 
 jest.mock('@folio/stripes/core', () => ({
   ...jest.requireActual('@folio/stripes-core'),
