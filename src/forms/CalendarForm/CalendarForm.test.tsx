f0f96cc06f17b2f08489ff4ccddcbf7ec352442c
William <wmholland@crimson.ua.edu>
2022-10-04 09:54:51 -0500

    Calendar Form increased coverage

---
+++ src/forms/CalendarForm/CalendarForm.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import * as Calendars from '../../test/data/Calendars';
+import withIntlConfiguration from '../../test/util/withIntlConfiguration';
+import CreateCalendarForm from './CalendarForm';
+import DataRepository from '../../data/DataRepository';
+import * as ServicePoints from '../../test/data/ServicePoints';
+
+
+// eslint-disable-next-line func-names
+window.matchMedia = window.matchMedia || function () {
+  return {
+    matches: false,
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    addListener() {},
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    removeListener() {}
+  };
+};
+
+const mutators = {
+  create: jest.fn(),
+  update: jest.fn(),
+  delete: jest.fn(),
+  dates: jest.fn(),
+  getUser: jest.fn()
+};
+
+
+describe('CreateCalendarForm', () => {
+  describe('idk whats going on', () => {
+    const dataRepository = new DataRepository([Calendars.SPRING_SP_1_2], [ServicePoints.SERVICE_POINT_1_DTO], mutators);
+    const closeParentLayer = jest.fn();
+    const setIsSubmitting = jest.fn();
+    const submitter = jest.fn();
+    it('renders', () => {
+      render(
+        withIntlConfiguration(
+          <CreateCalendarForm closeParentLayer={closeParentLayer} dataRepository={dataRepository} submitAttempted setIsSubmitting={setIsSubmitting} submitter={submitter} />
+        )
+      );
+      expect(screen.getByText('Calendar name')).toBeInTheDocument();
+    });
+  });
+});
