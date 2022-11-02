0c0b3be00150613d6146805977e84af4cb10add0
Faruq Yusuff <faruqyusuff437@gmail.com>
2022-11-02 13:33:01 -0500

    added helper Cypress commands - login, loginAsAdmin, openCalendarSettings, createCalendar, deleteCalendar

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'cypress';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+dotenv.config();
 
 export default defineConfig({
   fixturesFolder: 'src/test/cypress/fixtures',
+  env: {
+    // stripes username and password
+    diku_login: process.env.DIKU_USERNAME,
+    diku_password: process.env.DIKU_PASSWORD,
+    okapi_tenant: 'diku',
+    calendar_api_url: 'https://folio-dev-bama-okapi.ci.folio.org/calendar/calendars'
+  },
   e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return config;
     },
-    supportFile: 'src/test/cypress/support/e2e.ts'
+    supportFile: 'src/test/cypress/support/e2e.ts',
+    downloadsFolder: 'src/test/cypress/downloads',
+    specPattern: 'src/test/cypress/tests/*.cy.{js,jsx,ts,tsx}',
   },
 });
--- src/test/cypress/global.d.ts
+++ src/test/cypress/global.d.ts
@@ -1,7 +1,42 @@
 /// <reference types="cypress" />
+interface TestHour {
+    calendarId: string;
+    startDate: string;
+    startTime: string;
+    endDay: string;
+    endTime: string;
+}
+
+interface TestException {
+    calendarId: string;
+    name: string;
+    startDate: string;
+    endDate: string;
+    openings: {
+        exceptionId: string,
+        startDate: string;
+        startTime: string;
+        endDay: string;
+        endTime: string;
+    }[]
+}
+
+interface TestCalendarReqBody{
+    name: string;
+    startDate: string;
+    endDate: string;
+    assignments: string[];
+    normalHours: TestHour[];
+    exceptions: TestException[];
+}
+
 
 declare namespace Cypress {
     interface Chainable {
-        getBySel(dataTestAttribute: string, args?: any): Chainable<JQuery<HTMLElement>>;
+        login(username: string, password: string): void;
+        loginAsAdmin(): void;
+        openCalendarSettings(isLoggedIn?: boolean): void;
+        createCalendar(reqBody: TestCalendarReqBody, callback: (res: Response<any>) => void): void;
+        deleteCalendar(calendarID: string, callback?: (res: Response<any>) => void): void;
     }
 }
