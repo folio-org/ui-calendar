0dcc02a3e061ba1782318744326447310478b507
Noah Overcash <ncovercash@ua.edu>
2023-02-15 14:25:12 -0600

    UICAL-262: Fix over-aggressive validation of timepicker responses (#470)

--- src/forms/CalendarForm/validation/validateDateTime.isTimeProper.test.ts
+++ src/forms/CalendarForm/validation/validateDateTime.isTimeProper.test.ts
@@ -25,6 +25,8 @@ test('Proper 12-hour time values returns true', () => {
   expect(isTimeProper(localeTimeFormat12, '13:30:00', '1:30 PM')).toBe(true);
   expect(isTimeProper(localeTimeFormat12, '13:30', '01:30 PM')).toBe(true);
   expect(isTimeProper(localeTimeFormat12, '13:30:00', '01:30 PM')).toBe(true);
+  expect(isTimeProper(localeTimeFormat12, '13:30', '13:30')).toBe(true);
+  expect(isTimeProper(localeTimeFormat12, '03:30', '03:30')).toBe(true);
   expect(isTimeProper(localeTimeFormat12, '03:30', '03:30 AM')).toBe(true);
   expect(isTimeProper(localeTimeFormat12, '03:30', '3:30 AM')).toBe(true);
   expect(isTimeProper(localeTimeFormat12, '3:30', '03:30 AM')).toBe(true);
@@ -44,16 +46,14 @@ test('Invalid 24-hour time values returns false', () => {
   expect(isTimeProper(localeTimeFormat24, '13:30', 'foo')).toBe(false);
   expect(isTimeProper(localeTimeFormat24, '03:30', '25:29')).toBe(false);
   expect(isTimeProper(localeTimeFormat24, '03:30', '3:61')).toBe(false);
-  expect(isTimeProper(localeTimeFormat24, '3:30', '03:30 AM')).toBe(false);
   expect(isTimeProper(localeTimeFormat24, '3:30', '15:30')).toBe(false);
   expect(isTimeProper(localeTimeFormat24, '3:30', '2:20')).toBe(false);
   expect(isTimeProper(localeTimeFormat24, '3:30', '23:20')).toBe(false);
 });
 
-test('Invalid 12-hour time values returns true', () => {
+test('Invalid 12-hour time values returns false', () => {
   expect(isTimeProper(localeTimeFormat12, '13:30', '')).toBe(false);
   expect(isTimeProper(localeTimeFormat12, '13:30', 'foo')).toBe(false);
-  expect(isTimeProper(localeTimeFormat12, '13:30', '13:30')).toBe(false);
   expect(isTimeProper(localeTimeFormat12, '13:30', '13:30 AM')).toBe(false);
   expect(isTimeProper(localeTimeFormat12, '03:30', '25:29')).toBe(false);
   expect(isTimeProper(localeTimeFormat12, '03:30', '3:61')).toBe(false);
