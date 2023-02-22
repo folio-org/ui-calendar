eda7340c57f7a7fa17d6dbf887abff6a3e17a620
Faruq Yusuff <faruqyusuff437@gmail.com>
2023-02-22 14:04:52 -0600

    removed E2E cypress tests

--- src/test/cypress/interactors/headline.js
+++
@@ -1,5 +0,0 @@
-import HTML from '@folio/stripes-testing/interactors/baseHTML';
-
-export default HTML.extend('headline')
-  .selector('[class^="headline-"]')
-  .locator(el => el.textContent);
--- src/test/cypress/support/e2e.ts
+++
@@ -1,6 +0,0 @@
-// Import commands.js using ES2015 syntax:
-import './commands';
-
-// adding of methods do and expect
-import '@interactors/with-cypress';
-
