a7646d79dda3d280a1f0075e437eb8b81711d697
danetsao <danetsaoo@gmail.com>
2023-11-06 16:14:43 -0600

    Add typing for TitleManager

--- src/typings/stripes/core.d.ts
+++ src/typings/stripes/core.d.ts
@@ -1,5 +1,6 @@
 import type ky from 'ky';
 import type { Context, FunctionComponent, ReactNode } from 'react';
+import React from 'react';
 
 export type CalloutContextType = {
   sendCallout: (args: {
@@ -12,6 +13,9 @@ export const CalloutContext: Context<CalloutContextType>;
 
 export interface StripesType {
   hasPerm: (perm: string) => boolean;
+  config?: {
+    platformName?: string;
+  };
 }
 
 export function useStripes(): StripesType;
@@ -19,6 +23,14 @@ export function useStripes(): StripesType;
 export const IfPermission: FunctionComponent<{
   perm: string;
   children: ReactNode | ((props: { hasPermission: boolean }) => ReactNode);
+}>;
+
+export const TitleManager: React.Component<{
+  children?: ReactNode;
+  prefix?: string;
+  page?: string;
+  record?: string;
+  stripes?: StripesType;
 }>;
 
 export function useOkapiKy(): typeof ky;
